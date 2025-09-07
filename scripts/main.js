5455c00459ac032e90635aa4975fe915a0212961
phgdet <phgdet@gmail.com>
2025-09-07 12:43:55 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+  });
+
+// const myImage = document.querySelector("img");
+
+// myImage.addEventListener("click", () => {
+//   const mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/firefox-icon.png") {
+//     myImage.setAttribute("src", "images/FisgL45aUAA3MHN.jpeg");
+//   } else {
+//     myImage.setAttribute("src", "images/firefox-icon.png");
+//   }
+// });
